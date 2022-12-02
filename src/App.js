cf53894532baba4961c0b37763ab5fdc60c9b6f0
Gorthi-Sarvani <sarvanilakshmidevi@gmail.com>
2022-12-02 11:51:19 +0530

    reactjs-Events

---
+++ src/App.js
@@ -0,0 +1,27 @@
+/*import ClassComponent from "./ClassComponent"
+import Component2 from "./Component2"
+function App(){
+  return(
+    <section>
+      <h1>Good morning</h1>
+      <p>hello guys</p>
+      <img src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg" alt=""/>
+      <ClassComponent/>
+      <Component2/>
+    </section>
+  )
+}
+ export default App;*/
+
+//import PropsExample from './PropsExample'
+import Events from './Events'
+function App(){
+  return(
+      <div>
+      <Events/>
+        {/*<h1>This is about Props</h1>
+        <PropsExample name="thanu" age="21"/>*/}
+      </div>
+    )
+}
+export default App
---
+++ src/Component2.js
@@ -0,0 +1,11 @@
+import React from "react"
+class Component2 extends React.Component{
+	render(){
+		return(
+			<div>
+				from class Component2
+			</div>
+		)	
+	}
+}
+export default Component2
